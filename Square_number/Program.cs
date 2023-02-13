eb3fe07ede04d82d8de91c01056ebe6ba30a32ba
Abvorobey <abvorobey@gmail.com>
2023-02-13 20:52:05 +0300

    Квадрат числа

---
+++ Square_number/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу,
+ которая на вход принимает число, 
+ а на выходе выдает его квадрат*/
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine());
+Console.Write ("Квадрат этого числа будет: ");
+Console.WriteLine (a*a);
+Console.WriteLine ($"Я же сказал, что квадрат данного числа будет {a*a}");
+Console.WriteLine ($"Чего тут не понятного?, {Convert.ToInt32(Math.Pow(a,2))}");
+
+
