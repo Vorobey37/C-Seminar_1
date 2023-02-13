cb7a54efcfeb4411a8c3ec17914504b0085179c6
Abvorobey <abvorobey@gmail.com>
2023-02-13 23:43:24 +0300

    N, c учетом проверки на натуральность)

---
+++ While_N_from_-N_to_N/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая на входе принимает одно натуральное число N,
+а на выходе показывает все целые числа от -N до N*/
+Console.Clear();
+Console.WriteLine("Введите натуральное число: ");
+int a = int.Parse(Console.ReadLine());
+while (a<=0)
+{Console.WriteLine("Это не натуральное число!!!");
+Console.WriteLine("Введите НАТУРАЛЬНОЕ число: ");
+a = int.Parse(Console.ReadLine());}
+int b = -a;
+while(b<=a)
+{Console.Write($"{b} ");
+b++;}
+
+
+
+
+
+
