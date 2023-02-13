68ae5bb9b5d37adbd4eb7c397bb2e9661c6de990
Abvorobey <abvorobey@gmail.com>
2023-02-13 21:14:47 +0300

    Проверка квадрата

---
+++ Square_verification/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая на вход принимает два числа и проверяет,
+является ли первое число квадратом второго*/
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine ());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine ());
+if (a==b*b)
+{
+    Console.WriteLine("Танечка, ты молодец!");
+    Console.WriteLine($"Ведь число {a} - это {b} в квадрате!");
+}
+else 
+{
+   Console.WriteLine($"Мне жаль, но число {a} - это не {b} в квадрате");
+}
+
+
+
+
+
