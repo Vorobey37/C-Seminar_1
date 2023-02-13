b34a702568391acb10e1ec463f5bfd5e96dbd8c5
Abvorobey <abvorobey@gmail.com>
2023-02-13 22:47:15 +0300

    Дни недели

---
+++ Week_days/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Напишите программу, которая будет выдавать 
+название дня недели по заданному номеру*/
+Console.Clear();
+Console.WriteLine("Введите номер: ");
+int a = int.Parse(Console.ReadLine());
+if (a<1||a>7)
+{Console.WriteLine($"{a} - Дней в неделе всего семь!!!");
+return; // return для того, чтобы выйти из программы
+}
+if (a==1)
+{Console.WriteLine($"{a} - это понедельник!");}
+if (a==2)
+{Console.WriteLine($"{a} - это вторник!");}
+if (a==3)
+{Console.WriteLine($"{a} - это среда!");}
+ if (a==4)
+{Console.WriteLine($"{a} - это четверг!");}
+if (a==5)
+{Console.WriteLine($"{a} - это пятница!");}
+if (a==6)
+{Console.WriteLine($"{a} - это суббота!");}
+if (a==7)
+{Console.WriteLine($"{a} - это воскресенье!");}
+
+
+
+
+
